1d3e0bf4a0eff2b6aeedcaa97546bcaa7f95d603
Przemysław Kaleta <p.kaleta4@student.uw.edu.pl>
2025-01-01 22:17:51 +0100

    Added converter from h5 file to proper Pytorch Dataset

---
+++ h5reader.py
@@ -0,0 +1,19 @@
+import h5py
+import torch
+from torch.utils.data import Dataset
+
+class HDF5Dataset(Dataset):
+    def __init__(self, hdf5_file):
+        self.file = hdf5_file
+        with h5py.File(self.file, 'r') as hf:
+            self.length = hf['data'].shape[0]
+
+    def __len__(self):
+        return self.length
+
+    def __getitem__(self, idx):
+        with h5py.File(self.file, 'r') as hf:
+            # Load a single sample lazily
+            data = hf['data'][idx]
+            label = hf['labels'][idx]
+        return torch.tensor(data, dtype=torch.float32), torch.tensor(label, dtype=torch.float32)
