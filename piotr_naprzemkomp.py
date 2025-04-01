7431129d4b04a50760df3cf8c81931aa83dfc451
Przemysław Kaleta <p.kaleta4@student.uw.edu.pl>
2025-04-01 22:27:56 +0200

    Added GetModel function to make models easier. Removed unused imports. Added snorm_RMS loss function. This file should be ready to run on ICM computer

--- piotr_naprzemkomp.py
+++ piotr_naprzemkomp.py
@@ -1,4 +1,3 @@
-# %%
 import h5py
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "1"
@@ -6,58 +5,111 @@ import tensorflow as tf
 tf.get_logger().setLevel('ERROR')
 import tensorflow as tf
 import numpy as np
-import math
 import csv
-import time
-from tensorflow.keras import layers, models, Model, Input, regularizers
-from tensorflow.keras.callbacks import EarlyStopping
-from tensorflow.keras.layers import Lambda
-from tensorflow.keras.utils import plot_model
-import matplotlib.pyplot as plt
-from matplotlib.image import imread
-import matplotlib.image as mpimg
-from scipy.optimize import curve_fit
-from scipy.stats import norm
-from matplotlib.gridspec import GridSpec
+from tensorflow.keras import layers, models, Model, Input, regularizers # type: ignore
+from tensorflow.keras.callbacks import EarlyStopping # type: ignore
+from tensorflow.keras.layers import Lambda # type: ignore
+from tensorflow.keras.utils import plot_model # type: ignore
 from datetime import datetime
 
-# %%
+"""     GLOBALS     """
 hdf5_file = "energy_data.h5"
 test_hdf5_file = "angle_data.h5"
-klasyka_file='klasyka_z_regresja.csv'
-klasyka_file2='TE_RMSE_sigma.txt'
 
-# %%
 p=1
-class_coeff=[1.149,1.13]
 Energies=np.sort(np.array([130,140,40,30,200,100,90,170,120,150,190,70,160,60,80,180,110,50,25,75,125]))
 
-# %%
+""" FUNCTIONS       """
 # Define the generator function
 def hdf5_generator(hdf5_file):
     with h5py.File(hdf5_file, 'r') as f:
-        data = f['data']  # Assuming data is (x, y, z) for each sample in the file
+        data = f['data']
         labels = f['labels']
         sum_energies = f['energy']
-        num_samples = data.shape[0]  # The number of samples in the dataset
+        num_samples = data.shape[0]
 
         for i in range(num_samples):
-            image = data[i]  # Shape of image: (x, y, z)
-            label = labels[i].astype(np.float32).reshape(1)  # Shape of label, depending on your task
-            sum_energy = sum_energies[i].astype(np.float32).reshape(1)  # Convert to float32 for TensorFlow compatibility
-            # Add color channel dimension to the image (shape becomes (x, y, z, 1))
-            image = np.expand_dims(image, axis=-1)  # Adding the color channel (1 for grayscale)
-            # Yield the image and label
+            image = data[i]
+            label = labels[i].astype(np.float32).reshape(1) # casting for safety
+            sum_energy = sum_energies[i].astype(np.float32).reshape(1) # casting for safety
+            image = np.expand_dims(image, axis=-1)
+
             yield (image, sum_energy), label
 
-# Define the dataset pipeline
+def snorm_RMS(y_true, y_pred):
+    """Square Root Normalized Root Mean Square (RMS) divided by sqrt(y_true)."""
+    y_true = tf.cast(y_true, tf.float32)
+    y_pred = tf.cast(y_pred, tf.float32)
+    rms = tf.square(y_true - y_pred)
+    snorm_rms = rms / tf.maximum(tf.sqrt(y_true), 1e-10)  # Avoid division by zero
+    return tf.sqrt(tf.reduce_mean(snorm_rms, axis=-1))
+
+def GetModel(convFilters:np.array,denseNeurons:np.array):
+    # Input Layer
+    input_3d = Input(shape=(20, 110, 11, 1,), name='3D_Input')
+    input_energies = Input(shape=(1,), name='Energies_Input')
+    
+    # 3D CNN Path
+    x = input_3d
+    for nFeatures in convFilters:
+        x = layers.Conv3D(nFeatures, kernel_size=(3, 3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(0.01))(x)
+        x = layers.MaxPooling3D(pool_size=(2, 2, 2))(x)
+
+    # Flatten 3D features
+    x = layers.Flatten()(x)
+
+    combined = layers.Concatenate(axis=-1)([x, input_energies])  # Concatenate along last axis
+
+    # Fully Connected Layers
+    x = combined
+    for nNeurons in denseNeurons:
+        x = layers.Dense(nNeurons, activation='relu')(x)
+
+    # Output Layer
+    output = layers.Dense(1, activation='linear')(x)
+
+    # Define the Model Important to have two imputs
+    model = Model(inputs=[input_3d,input_energies], outputs=output)
+
+    #compile the model with no metrics for now
+    model.compile(optimizer='adam',
+                loss=snorm_RMS)
+    return model
+
+def SaveModel(a,b,c,d, filename,model):
+    combined_array = np.vstack((a, b, c, d)).T
+
+    # Step 2: Define the header for the CSV file
+    header = ['ME', 'MAE', 'normMAE', 'RMS']
+
+    # Step 3: Write the combined array to a CSV file
+    with open(filename+'.csv', mode='w', newline='') as file:
+        writer = csv.writer(file)
+        # Write the header
+        writer.writerow(header)
+        # Write the data
+        writer.writerows(combined_array)  
+    model.save('model+'+filename)
+
+def Metrics(E_true,E_pred,Energies):
+    RMS=np.zeros((len(Energies)))
+    ME=np.zeros((len(Energies)))
+    MAE=np.zeros((len(Energies)))
+    for i in range(0,len(Energies)):
+        RMS[i]=np.sqrt(np.mean((E_true[E_true==Energies[i]]-E_pred[E_true==Energies[i]])**2))
+        ME[i]=np.mean(E_pred[E_true==Energies[i]]-E_true[E_true==Energies[i]])
+        MAE[i]=np.mean(np.abs(E_pred[E_true==Energies[i]]-E_true[E_true==Energies[i]]))
+    return RMS, ME, MAE, MAE/np.sqrt(Energies)
+
+"""     DATASET LOADING     """
+
 dataset = tf.data.Dataset.from_generator(
     hdf5_generator,
     args=[hdf5_file],  # Provide the path to your HDF5 file here
     output_signature=(
-        (tf.TensorSpec(shape=(20, 110, 11, 1), dtype=tf.float32, name='x'),  # Shape with added color channel
-        tf.TensorSpec(shape=(1,), dtype=tf.float32,name = 'energy')),  # Label shape
-        tf.TensorSpec(shape=(1,), dtype=tf.int16,name = 'y')  # Sum energy shape
+        (tf.TensorSpec(shape=(20, 110, 11, 1), dtype=tf.float32, name='x'),
+        tf.TensorSpec(shape=(1,), dtype=tf.float32,name = 'energy')),
+        tf.TensorSpec(shape=(1,), dtype=tf.int16,name = 'y')
     )
 )
 
@@ -93,77 +145,44 @@ test_ds2=test_ds2.batch(32, drop_remainder=True).prefetch(tf.data.AUTOTUNE)
 
 print("Dane treningowe, walidacyjne i testowe ready")
 
-# %%
+"""     MAIN BODY   """
+
 early_stopping = EarlyStopping(
     monitor='val_loss',         # Metric to monitor (e.g., 'val_loss' or 'val_accuracy')
     patience=3,                 # Number of epochs with no improvement to wait before stopping
     restore_best_weights=True   # Restore model weights from the best epoch
 )
 
-# %%
-#Tu zdefiniuj model
-input_3d = Input(shape=(20, 110, 11, 1,), name='3D_Input')
+#Tu zdefiniuj modele
+models,names = [],[]
+models.append(GetModel(convFilters=[32,32,16],denseNeurons=[64,32,16]))
+models.append(GetModel(convFilters=[16,8,8],denseNeurons=[32,16,8]))
+names = ["BIG","SMALL"]
 
-input_energies = Input(shape=(1,), name='Energies_Input')
-# 1D CNN Path
+for model, name in zip(models,names):
+    try:
+        os.mkdir(f"{name}_logs")
+    except FileExistsError:
+        pass
+    logs = f"{name}_logs/"+ datetime.now().strftime("%Y%m%d-%H%M%S")
+    tboard_callback = tf.keras.callbacks.TensorBoard(log_dir= logs,
+                                                    histogram_freq=1,
+                                                    profile_batch='420,430')
+    with tf.device('/GPU:0'):
+        history = model.fit(
+            train_dataset,
+            validation_data=val_dataset,
+            epochs=20,
+            callbacks=[early_stopping,tboard_callback],
+            verbose=0
+            )
+        #Generowanie danych
+        y_pred=model.predict(test_ds).flatten()
+        y_true = np.array([label.numpy() for _, label in test_ds]).flatten()
+        y_pred2=model.predict(test_ds2).flatten()
+        y_true2 = np.array([label.numpy() for _, label in test_ds2]).flatten()
+        y_true=np.concatenate((y_true,y_true2),axis=0)
+        y_pred=np.concatenate((y_pred,y_pred2),axis=0)
 
-# 3D CNN Path
-x = layers.Conv3D(32, kernel_size=(3, 3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(0.01))(input_3d)
-x = layers.MaxPooling3D(pool_size=(2, 2, 2))(x)
-
-x = layers.Conv3D(32, kernel_size=(3, 3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(0.01))(x)
-x = layers.MaxPooling3D(pool_size=(2, 2, 2))(x)
-
-x = layers.Conv3D(16, kernel_size=(3, 3, 3), activation='relu', padding='same', kernel_regularizer=regularizers.l2(0.01))(x)
-x = layers.MaxPooling3D(pool_size=(2, 2, 2))(x)
-
-# Flatten 3D features
-x = layers.Flatten()(x)
-
-combined = layers.Concatenate(axis=-1)([x, input_energies])  # Concatenate along last axis
-
-# Fully Connected Layers
-fc = layers.Dense(64, activation='relu')(combined)
-fc = layers.Dense(32, activation='relu')(fc)
-fc = layers.Dense(16, activation='relu')(fc)
-
-# Output Layer
-output = layers.Dense(1, activation='linear')(fc)
-
-# Define the Model Important to have two imputs
-model = Model(inputs=[input_3d,input_energies], outputs=output)
-
-#compile the model with no metrics for now
-model.compile(optimizer='adam',
-              loss='mean_squared_error')
-            
-model.summary()
-plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=True)
-
-# %%
-csv_logger = tf.keras.callbacks.CSVLogger("training_log5.csv", append=True)
-
-logs = "logs/"+ datetime.now().strftime("%Y%m%d-%H%M%S")
-tboard_callback = tf.keras.callbacks.TensorBoard(log_dir= logs,
-                                                 histogram_freq=1,
-                                                 profile_batch='420,430')
-
-
-history = model.fit(
-    train_dataset,
-    validation_data=val_dataset,
-    shuffle=True,
-    epochs=20,                 # Set a high number of epochs; EarlyStopping will stop early
-    callbacks=[early_stopping,csv_logger,tboard_callback],  # Add EarlyStopping to callbacks
-    verbose=1
-    )
-
-#Generowanie danych
-y_pred=model.predict(test_ds).flatten()
-y_true = np.array([label.numpy() for _, label in test_ds]).flatten()
-y_pred2=model.predict(test_ds2).flatten()
-y_true2 = np.array([label.numpy() for _, label in test_ds2]).flatten()
-y_true=np.concatenate((y_true,y_true2),axis=0)
-y_pred=np.concatenate((y_pred,y_pred2),axis=0)
-
-model.save('piotr_no_metrics_maybefaster.h5')
+    RMS, ME, MAE, normMAE = Metrics(y_true, y_pred, Energies)
+    SaveModel(ME,MAE,normMAE,RMS,name, model)
